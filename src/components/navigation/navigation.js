6b39501e523898c6ccc63d6f090d3a183cee60e8
XLpacman805 <XLpacman805@gmail.com>
2022-01-22 16:14:33 -0800

    Created navigation bar

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,27 +1,61 @@
 import './navigation.scss';
 
+const navigationData = [
+    {
+        id: 'nav-me',
+        text: 'Me'
+    },
+    {
+        id: 'nav-projects',
+        text: 'Projects'
+    },
+    {
+        id: 'nav-contact',
+        text: 'Contact'
+    }
+];
+
+const createListItems = (navigationData, activeId) => {
+    return navigationData.map(item => {
+        return `
+            <li ${item.id === activeId ? 'class="active"' : ''} id="${item.id}">
+                ${item.text}
+            </li>
+        `;
+    }).join('');
+};
+
 // create custom element
 class Navigation extends HTMLElement {
     constructor() {
         super();
         this.render = this.render.bind(this);
+        this.active = navigationData[0].id;
     }
 
     connectedCallback() {
         this.render();
     }
 
+    handleClick(e) { 
+        if (e.target.tagName === 'LI' && e.target.id !== this.active) {
+            this.active = e.target.id;
+            this.render();
+        }
+    }
+
     render() {
         this.innerHTML = `
             <nav>
-                <h1 class="home">JohnnyMezaDev.com</h1>
+                <h1 class="home">johnnyMezaDev.com</h1>
                     <ul class="root-ul">
-                        <li class="active">Me</li>
-                        <li>Projects</li>
-                        <li>Contact</li>
+                        ${createListItems(navigationData, this.active)} 
                     </ul>
             </nav>
         `;
+
+        this.removeEventListener('click', this.handleClick);
+        this.addEventListener('click', this.handleClick);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './styles.scss';
 import './components/navigation/navigation.js';
+
+
+
+class App extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+            <navigation-jmdev></navigation-jmdev>
+        `;
+    }
+}
+
+customElements.define('app-jmdev', App);
